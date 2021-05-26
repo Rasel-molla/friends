87c68b8e88663f13028c849ec68aaf6ba27b45bd
Rasel-molla <raselabir107@gmail.com>
2021-05-27 02:53:38 +0600

    comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import NoMatch from './components/NoMatch/NoMatch';
+import FriendDetail from './components/FriendDetail/FriendDetail';
 
 function App() {
+
+
+
   return (
     <div className="App">
-<h1> This Now </h1>
 
+
+
+<Router>
+<Switch>
+  <Route path="/Home">
+  <Home></Home>
+  </Route>
+
+  <Route path="/friend/:friendId">
+<FriendDetail></FriendDetail>
+  </Route>
+
+  <Route path="/">
+  <Home></Home>
+  </Route>
+
+<Route exact path="*" >
+<NoMatch></NoMatch>
+</Route>
+</Switch>
+
+</Router>
     </div>
   );
 }
---
+++ src/components/Friends/Friend.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Friend = (props) => {
+
+    const {name,email,id } = props.friend;
+
+
+    const friendStyle ={
+  border:'1px solid grey',
+  margin:'20px',
+  padding:'20px',
+  borderRadius:'20px'
+
+    }
+    return (
+        <div style={friendStyle }>
+            <h2>Name:{name}</h2>
+            <p>email:{email}</p>
+  <Link to={`/friend/${id} `}>
+                
+                <button>Show detail{id}</button>
+                </Link>
+
+
+        </div>
+    );
+};
+
+export default Friend;
